9df14ac4fb945c9af3d59a9c48713eba80559ee4
exploitxd <60840691+exploitxd@users.noreply.github.com>
2022-10-31 00:35:13 +0530

    11th question

--- project-1.c
+++ project-1.c
@@ -144,4 +144,17 @@ printf("ASCII value of %c is %d",ch,ch);
 return 0;
 }
 
+// write a program to input the number in days and convert it into years, weeks and days.
+#include<stdio.h>
+int main()
+{
+int days,years,weeks;
+printf("Enter number of days: ");
+scanf("%d",&days);
+years=days/365;
+weeks=(days%365)/7;
+days=days-((years*365)+(weeks*7));
+printf("Years= %d Weeks= %d Days= %d",years,weeks,days);
+return 0;
+}
 
